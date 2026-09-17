8fa4764974c4f38d9fd11e847a683efb2c0da09c
RaifeRiver <cade@blazestone.io>
2026-09-17 16:29:16 +0100

    Add comments to PIDController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -10,6 +10,15 @@ public class PIDController {
 	private float mIntegral;
 	private float mLastError;
 
+	/**
+	 * Creates a new PID controller
+	 *
+	 * @param kp weight for p
+	 * @param ki weight for i
+	 * @param kd weight for d
+	 * @param min minimum output value
+	 * @param max maximum output value
+	 */
 	public PIDController(float kp, float ki, float kd, float min, float max) {
 		mKP = kp;
 		mKI = ki;
@@ -18,6 +27,14 @@ public class PIDController {
 		mMax = max;
 	}
 
+	/**
+	 * Updates the PID controller
+	 *
+	 * @param current the current value
+	 * @param target the target value
+	 * @param dt delta time (time in seconds since last update)
+	 * @return the output value
+	 */
 	public float update(float current, float target, float dt) {
 		float error = target - current;
 
